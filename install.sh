d7e573491c40398a08a63eb84b33b47ef5ab9c8b
Rémy Peru <peru.remy@gmail.com>
2019-04-14 10:05:41 +0200

    Exit on errors

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+
 DOWNLOAD_URL="https://github.com/kilbiller/work-setup/archive/master.tar.gz"
 test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
 
@@ -13,20 +14,20 @@ else
 fi
 
 # Install fonts
-mkdir -p $HOME/.fonts \
-&& cp -r $TMPDIR/fonts $HOME/.fonts \
-&& fc-cache -f -v
+mkdir -p $HOME/.fonts
+cp -r $TMPDIR/fonts $HOME/.fonts
+fc-cache -f -v
 
 # Install zsh
-sudo apt-get install -y zsh \
-&& curl -sL git.io/antibody | sh -s \
-&& cp -rf $TMPDIR/.zshrc $HOME/.zshrc
+sudo apt-get install -y zsh
+curl -sL git.io/antibody | sh -s
+cp -rf $TMPDIR/.zshrc $HOME/.zshrc
 
 # Install git
-sudo apt-get install -y git \
-&& cp -rf $TMPDIR/.gitconfig $HOME/.gitconfig
+sudo apt-get install -y git
+cp -rf $TMPDIR/.gitconfig $HOME/.gitconfig
 
 # Install hyper
-curl -sL https://github.com/zeit/hyper/releases/download/2.1.2/hyper_2.1.2_amd64.deb -o $TMPDIR/hyper.deb \
-&& sudo apt --fix-broken install -y $TMPDIR/hyper.deb \
-&& cp -rf $TMPDIR/.hyper.js $HOME/.hyper.js
+curl -sL https://github.com/zeit/hyper/releases/download/2.1.2/hyper_2.1.2_amd64.deb -o $TMPDIR/hyper.deb
+sudo apt --fix-broken install -y $TMPDIR/hyper.deb
+cp -rf $TMPDIR/.hyper.js $HOME/.hyper.js
