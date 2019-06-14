54e38f264fc1f5d29d366c387d704681595ef4c3
Rémy Peru <peru.remy@gmail.com>
2019-06-14 20:31:13 +0200

    Fix docker install

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ cp -rf $TMPDIR/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 # Install docker-compose
