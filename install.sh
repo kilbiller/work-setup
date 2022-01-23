c23d3c87d04e74a831b921815ec4642f7faec4d1
Rémy Peru <peru.remy@gmail.com>
2022-01-23 15:57:05 +0100

    Upgrade install script

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 set -e
 
 DOWNLOAD_URL="https://github.com/kilbiller/work-setup/archive/master.tar.gz"
-DOCKER_COMPOSE_VERSION=1.28.6
-NODEJS_VERSION=14
+DOCKER_COMPOSE_VERSION=2.2.3
+NODEJS_VERSION=16
 PHP_VERSION=7.4
-KUBERNETES_VERSION=1.17.0
+KUBERNETES_VERSION=1.21.0
 
 # Create temp directory
 test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
@@ -28,14 +28,12 @@ mkdir -p "$HOME"/.fonts
 cp -r "$TMPDIR"/fonts "$HOME"/.fonts
 fc-cache -v
 
-# Install zsh & plugin manager
-sudo apt-get install -y zsh
-curl -sfL git.io/antibody | sudo -E sh -s - -b /usr/local/bin
+# Install fish
+sudo add-apt-repository -y --update ppa:fish-shell/release-3
+sudo apt install fish
 
-cp -rf "$TMPDIR"/.zshrc "$HOME"/.zshrc
-
-# Change default shell to zsh
-sudo chsh $USER -s $(which zsh)
+# Change default shell
+sudo chsh "$USER" -s "$(which fish)"
 
 # vim
 sudo apt-get install -y vim
@@ -44,82 +42,24 @@ sudo apt-get install -y vim
 sudo apt-get install -y git
 
 # Set git config if not already
-if test -z $(git config --global --get user.email); then
+if test -z "$(git config --global --get user.email)"; then
   echo "What is your git email ?"
-  read git_email
+  read -r git_email
   echo "What is your git name ?"
-  read git_name
+  read -r git_name
 
-  git config --global user.email $git_email
-  git config --global user.name $git_name
+  git config --global user.email "$git_email"
+  git config --global user.name "$git_name"
 fi
 git config --global core.editor vim
 
-# gnome terminal (ubuntu default)
-curl -sL https://raw.githubusercontent.com/arcticicestudio/nord-gnome-terminal/develop/src/nord.sh | bash
-
-set_default_profile_by_name() {
-  local name=$1
-  local profiles="$(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "[]\',")"
-  for uuid in "$profiles"; do
-    if [[ "$(gsettings get org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"${uuid}"/ visible-name)" == "'$name'" ]]; then
-      gsettings set org.gnome.Terminal.ProfilesList default "${uuid}"
-	    return 0
-    fi
-  done
-}
-
-set_default_profile_by_name "Nord"
-gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-
-# Profile settings
-GNOME_TERMINAL_PROFILE=`gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}'`
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ font 'Hack 14'
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ use-system-font false
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ audible-bell false
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ scrollbar-policy 'never'
-
-# Alternative option (load entire config as file)
-#cat <<EOT | dconf load /org/gnome/terminal/
-#[legacy]
-#default-show-menubar=false
-#headerbar=@mb false
-#schema-version=uint32 3
-#
-#[legacy/profiles:]
-#default='09dc1416-e8bd-43c6-a2ad-c37c017d3da2'
-#list=['09dc1416-e8bd-43c6-a2ad-c37c017d3da2']
-#
-#[legacy/profiles:/:09dc1416-e8bd-43c6-a2ad-c37c017d3da2]
-#audible-bell=false
-#background-color='#2E3440'
-#bold-color='#D8DEE9'
-#bold-color-same-as-fg=true
-#cursor-background-color='rgb(216,222,233)'
-#cursor-colors-set=true
-#cursor-foreground-color='rgb(59,66,82)'
-#font='Hack 14'
-#foreground-color='#D8DEE9'
-#highlight-background-color='rgb(136,192,208)'
-#highlight-colors-set=true
-#highlight-foreground-color='rgb(46,52,64)'
-#nord-gnome-terminal-version='0.1.0'
-#palette=['#3B4252', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#88C0D0', '#E5E9F0', '#4C566A', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#8FBCBB', '#ECEFF4']
-#scrollbar-policy='never'
-#use-system-font=false
-#use-theme-background=false
-#use-theme-colors=false
-#use-theme-transparency=false
-#use-transparent-background=false
-#visible-name='Nord'
-#EOT
-
 # google-chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+if ! grep -q microsoft /proc/version; then # Not in wsl
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  sudo apt-get update
+  sudo apt-get install -y google-chrome-stable
+fi
 
 # vscode
 if ! grep -q microsoft /proc/version; then # Not in wsl
@@ -144,18 +84,19 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker "$USER"
 
 # docker-compose
-echo "Installing docker-compose..."
-if test "$DOCKER_COMPOSE_VERSION" != $(docker-compose version --short); then
-sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
-  sudo chmod +x /usr/local/bin/docker-compose
+echo "Installing docker compose ${DOCKER_COMPOSE_VERSION}..."
+if test "$DOCKER_COMPOSE_VERSION" != "$(docker compose version --short)"; then
+  sudo mkdir -p /usr/local/lib/docker/cli-plugins
+  sudo curl -SL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+  sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
   echo "done"
 else
   echo "already installed"
 fi
 
 # kubectl
-echo "Installing kubectl..."
-if test "v${KUBERNETES_VERSION}" != $(kubectl version --client --short | awk '{print $3}'); then
+echo "Installing kubectl ${KUBERNETES_VERSION}..."
+if test "v${KUBERNETES_VERSION}" != "$(kubectl version --client --short | awk '{print $3}')"; then
   sudo curl -L "https://dl.k8s.io/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
   sudo chmod +x /usr/local/bin/kubectl
   echo "done"
@@ -186,7 +127,7 @@ latest_version=$(curl -s https://api.github.com/repos/kubernetes/minikube/releas
  head -n 1 |
  awk -F'/' '{print $8}'
 )
-if test $latest_version != $(minikube version --short | awk '{print $3}'); then
+if test "$latest_version" != "$(minikube version --short | awk '{print $3}')"; then
 curl -LO "https://storage.googleapis.com/minikube/releases/${latest_version}/minikube-linux-amd64" &&
   sudo install minikube-linux-amd64 /usr/local/bin/minikube &&
   rm minikube-linux-amd64
@@ -196,7 +137,7 @@ else
 fi
 
 # nodejs
-echo "Installing nodejs..."
+echo "Installing node-${NODEJS_VERSION}..."
 curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
@@ -208,7 +149,7 @@ sudo apt-get update
 sudo apt-get install -y yarn
 
 # php
-echo "Installing php..."
+echo "Installing php${PHP_VERSION}..."
 sudo add-apt-repository -y --update ppa:ondrej/php
 sudo apt-get install -y php${PHP_VERSION} php${PHP_VERSION}-dev php${PHP_VERSION}-curl php${PHP_VERSION}-gd php${PHP_VERSION}-mbstring php${PHP_VERSION}-xml php${PHP_VERSION}-zip
 
