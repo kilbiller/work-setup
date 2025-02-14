8584fa117cb5f3eb745b49570a8edd96310bfeb3
Rémy Peru <peru.remy@gmail.com>
2025-02-14 18:34:25 +0100

    Remove some softwares

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ DOWNLOAD_URL="https://github.com/kilbiller/work-setup/archive/master.tar.gz"
 DOCKER_COMPOSE_VERSION=2.7.0
 NODEJS_VERSION=22
 PHP_VERSION=8.3
-KUBERNETES_VERSION=1.23.0
 
 # Create temp directory
 test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
@@ -77,14 +76,6 @@ sudo mv "$TMPDIR"/exa /usr/local/bin/exa
 # fd
 sudo apt install -y fd-find
 
-# google-chrome
-if ! grep -q microsoft /proc/version; then # Not in wsl
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-  echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-  sudo apt-get update
-  sudo apt-get install -y google-chrome-stable
-fi
-
 # vscode
 if ! grep -q microsoft /proc/version; then # Not in wsl
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >"$TMPDIR"/microsoft.gpg
@@ -93,12 +84,6 @@ if ! grep -q microsoft /proc/version; then # Not in wsl
   sudo apt-get update
   sudo apt-get install -y code
 fi
-
-# gpg
-sudo apt-add-repository -y --update ppa:yubico/stable
-sudo apt-get install -y pcscd scdaemon gnupg2 pcsc-tools yubikey-manager
-mkdir -p "$HOME"/.gnupg
-cp -rf "$TMPDIR"/gpg-agent.conf "$HOME"/.gnupg/gpg-agent.conf
 
 # docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -118,68 +103,10 @@ else
   echo "already installed"
 fi
 
-# kubectl
-echo "Installing kubectl ${KUBERNETES_VERSION}..."
-if test "v${KUBERNETES_VERSION}" != "$(kubectl version --client --short | awk '{print $3}')"; then
-  sudo curl -L "https://dl.k8s.io/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
-  sudo chmod +x /usr/local/bin/kubectl
-  echo "done"
-else
-  echo "already installed"
-fi
-
-# completion
-kubectl completion fish > ~/.config/fish/completions/kubectl.fish
-
-# kustomize
-echo "Installing kustomize..."
-curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |
-  grep browser_download |
-  grep linux_amd64 |
-  cut -d '"' -f 4 |
-  grep /kustomize/v |
-  sort |
-  tail -n 1 |
-  xargs curl -L -o "$TMPDIR"/kustomize.tar.gz
-tar xzf "$TMPDIR"/kustomize.tar.gz -C "$TMPDIR"
-chmod +x "$TMPDIR"/kustomize
-sudo mv "$TMPDIR"/kustomize /usr/local/bin/kustomize
-
-# completion
-kustomize completion fish > ~/.config/fish/completions/kustomize.fish
-
-# minikube
-echo "Installing minikube..."
-latest_version=$(curl -s https://api.github.com/repos/kubernetes/minikube/releases |
- grep browser_download |
- grep minikube-linux-amd64 |
- cut -d '"' -f 4 |
- head -n 1 |
- awk -F'/' '{print $8}'
-)
-if test "$latest_version" != "$(minikube version --short | awk '{print $3}')"; then
-curl -LO "https://storage.googleapis.com/minikube/releases/${latest_version}/minikube-linux-amd64" &&
-  sudo install minikube-linux-amd64 /usr/local/bin/minikube &&
-  rm minikube-linux-amd64
-  echo "done"
-else
-  echo "already installed"
-fi
-
-# completion
-minikube completion fish > ~/.config/fish/completions/minikube.fish
-
 # nodejs
 echo "Installing node-${NODEJS_VERSION}..."
 curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
-# yarn
-echo "Installing yarn..."
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install -y yarn
 
 # php
 echo "Installing php${PHP_VERSION}..."
